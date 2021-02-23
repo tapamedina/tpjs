df575ffd8e925f92fdb05a54e20f3d46c813ad11
tapamedina <tapamedinadia@gmail.com>
2021-02-23 12:29:25 +0100

    [EXO5] Class Point

---
+++ exo5.js
@@ -0,0 +1,27 @@
+class Point {
+    constructor(x, y) {
+        this.x = x; 
+        this.y = y;
+    }
+    getX = () => {
+        return this.x;
+    }
+    setX = (x) => {
+        this.x = x;
+    }
+    getY = () => {
+        return this.x;
+    }
+    setY= (y) => {
+        this.y = y;
+    }
+
+    toString = () => {
+        return `Point(${this.x}, ${this.y})`;
+    }
+}
+//Test 
+let point = new Point(2, 4);
+console.log(point.toString());
+point.setX(4);
+console.log(point.getX());
